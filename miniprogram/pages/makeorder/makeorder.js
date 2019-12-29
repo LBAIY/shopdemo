b66875afc0159fbb0e1f1a1581098af4c1f286fe
MMX <15017662878@163.com>
2019-12-29 19:36:27 +0800

    feature- add pages ：makeorder and selectaddress

--- miniprogram/pages/makeorder/makeorder.js
+++ miniprogram/pages/makeorder/makeorder.js
@@ -17,7 +17,11 @@ Page({
   onLoad: function (options) {
 
   },
-
+  selectAddress: function() {
+    wx.navigateTo({
+      url: '../selectaddress/selectaddress',
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
