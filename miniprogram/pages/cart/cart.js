b987df51ab58055e4d0a2bf5befec04a3bcd09d7
grape11111 <2549107521@qq.com>
2019-12-29 11:42:21 +0800

    init

---
+++ miniprogram/pages/cart/cart.js
@@ -0,0 +1,212 @@
+// page/component/new-pages/cart/cart.js
+const app = getApp()
+var openid=''
+
+Page({
+  data: {
+    carts: [],               // 购物车列表
+    hasList: false,          // 列表是否有数据
+    totalPrice: 0,           // 总价，初始为0
+    selectAllStatus: true,    // 全选状态，默认全选
+    obj: { name: "hello" }
+  },
+
+  //获取用户openid
+  getOpenid() {
+    let that = this;
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        //console.log('[云函数] [login] user openid: ', res.result.openid)
+        openid = res.result.openid
+        //console.log('openid1',openid)
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+  },
+
+  onLoad(e) {
+    //------获取初始化购物车----
+    var self = this
+    const db = wx.cloud.database()
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then(res => {
+      db.collection('cart').where({
+        _openid: res.result.openid
+      }).get().then(res2 => {
+        console.log('res2', res2);
+        if (res2.data.length != 0)
+          this.setData({
+            carts: res2.data,
+            hasList: true
+          });
+      })
+    })
+    app.globalData.carts = this.carts
+    this.getTotalPrice()
+  },
+
+  onShow() {
+    //------获取初始化购物车----
+     var self = this
+     const db = wx.cloud.database()
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then(res => {
+      db.collection('cart').where({
+        _openid: res.result.openid
+      }).get().then(res2 => {
+        console.log('res2',res2);
+        if(res2.data.length!=0)
+        this.setData({
+          carts: res2.data,
+          hasList: true
+        });
+      })
+    })
+    app.globalData.carts=this.carts
+    /*this.getOpenid()
+    //不知道怎么获取openid
+    //this.getOpenid() //onload已经设置全局变量openid
+    app.getInfoWhere('cart', { _openid: openid },
+      res => {
+        console.log('res',res.data)
+        console.log('openid2', openid)
+        if(!res.data && res.data.length<=0){
+          console.log("购物车没有商品")
+        }
+        self.data.carts = res.data
+        app.globalData.carts = self.data.carts
+        //console.log('openid',app.globalData._openid)
+      }
+    )*/
+    //if (app.globalData.carts.length != 0) {
+    // if (carts.length!=0){
+    //   self.setData({
+    //     hasList: true,
+    //   });
+    // }
+    self.selectAll();
+    self.getTotalPrice();
+  },
+
+  onHide: function () {
+    var self = this
+    self.getTotalPrice();
+    self.selectAll();
+  },
+
+  /**
+   * 当前商品选中事件
+   */
+  selectList(e) {
+    var self = this
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    const selected = carts[index].selected;
+    carts[index].selected = !selected;
+    this.setData({
+      carts: carts
+    });
+    app.globalData.carts = carts
+    this.getTotalPrice();
+  },
+
+  /**
+   * 删除购物车当前商品
+   */
+  deleteList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    carts.splice(index, 1);
+    this.setData({
+      carts: carts
+    });
+    app.globalData.carts = carts
+    if (!carts.length) {
+      this.setData({
+        hasList: false
+      });
+    } else {
+      this.getTotalPrice();
+    }
+  },
+
+  /**
+   * 购物车全选事件
+   */
+  selectAll(e) {
+    let selectAllStatus = this.data.selectAllStatus;
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
+
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus;
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    });
+    app.globalData.carts = carts
+    this.getTotalPrice();
+  },
+
+  /**
+   * 绑定加数量事件
+   */
+  addCount(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    num = num + 1;
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    });
+    app.globalData.carts = carts
+    this.getTotalPrice();
+  },
+
+  /**
+   * 绑定减数量事件
+   */
+  minusCount(e) {
+    const index = e.currentTarget.dataset.index;
+    const obj = e.currentTarget.dataset.obj;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    if (num <= 1) {
+      return false;
+    }
+    num = num - 1;
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    });
+    app.globalData.carts = carts
+    this.getTotalPrice();
+  },
+
+  /**
+   * 计算总价
+   */
+  getTotalPrice() {
+    let carts = this.data.carts;                  // 获取购物车列表
+    let total = 0;
+    for (let i = 0; i < carts.length; i++) {         // 循环列表得到每个数据
+      if (carts[i].selected) {                     // 判断选中才会计算价格
+        total += carts[i].num * carts[i].price;   // 所有价格加起来
+      }
+    }
+    this.setData({                                // 最后赋值到data中渲染到页面
+      carts: carts,
+      totalPrice: total.toFixed(1)
+    });
+    app.globalData.carts = carts
+  }
+
+})
--- miniprogram/pages/type/type.js
+++ miniprogram/pages/type/type.js
@@ -1,48 +1,228 @@
 // miniprogram/pages/type.js
+const db = wx.cloud.database()
+var openid = ''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    allType:[],
+    resByType:[],
+    resOrByPrice:[],
+    num:446
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
-  //查询种类
-  selectType: function () {
+  //零食种类列表：分组查询（去重）
+  allType:function(){
     const db = wx.cloud.database()
-    // 查询当前用户所有的 counters
+    const $ = db.command.aggregate
+     db.collection('snacks').aggregate()
+      .group({
+        // 按 type 字段分组
+        _id: '$type',
+        // 每组有一个 totalAmount 字段，其值是组内所有记录的 Amount 字段的总和
+        totalAmount: $.sum('$amount')
+      })
+       .end(
+       {
+        success:res=>{
+          this.setData({
+            allType:res.list
+          })
+          console.log(res);
+        },
+         fail: err => {
+           console.log(err);
+         }
+      }
+      )
+  },
+
+  //按零食类别查询：拿到前端的点击类别--返回类型为数组
+  selectByType: function (e) {
+    const db = wx.cloud.database()
+    const type = e.currentTarget.dataset.index
     db.collection('snacks').where({
-      type: '糖果'
-    }).get({
+      type: this.data.allType[type]._id
+    })
+    .get({
       success: res => {
         this.setData({
-          queryResult: JSON.stringify(res.data, null, 2)
+          //queryResult: JSON.stringify(res.data, null, 2)
+          resByType:res.data
         })
         console.log('[数据库] [查询记录] 成功: ', res)
       },
       fail: err => {
-        wx.showToast({
-          icon: 'none',
-          title: '查询记录失败'
-        })
         console.error('[数据库] [查询记录] 失败：', err)
+      }
+        })
+    // db.collection('snacks').where({
+    //   type: "糖果"
+    // }).get().then(res => {
+    //   console.log(res);
+    // }).catch(err => {
+    //   console.log(err);
+    // })
+  },
+
+  //按价格排序（升序）
+  orderByPrice:function(){
+    db.collection('snacks')
+      .where({
+        type:"干果"
+      })
+      .orderBy('price', 'asc')
+      .get({
+        success: res => {
+          this.setData({
+            resOrByPrice: res.data
+          })
+          console.log('[数据库] [查询记录] 成功: ', res)
+        },
+        fail: err => {
+          console.error('[数据库] [查询记录] 失败：', err)
+        }
+      })
+  },
+
+  //计算价格
+  onPay:function(){
+    for(var i=0;i<cart.length;i++){
+      console.log(cart[i].num*cart[i].price)
+    }
+  },
+  
+  /*加入购物车
+  1.判断是否登陆(onload)
+  2.判断其购物车是否有该商品（有：数量+1；否则，新增记录）
+  */
+  addToCart:function(e){
+    const _ = db.command
+    var snack_id = e.currentTarget.dataset.index     //前端的data-index
+    var openid ="oUVpX45nNHbe9ELQRSKqlanjaiNE"
+    db.collection('cart').where({
+      snack_id: snack_id,
+      _openid: openid
+    }).get({
+      success(res) {
+        //如果查询到数据,则数量加1
+        if (res.data && res.data.length > 0) {
+          var id=res.data[0]._id
+          db.collection('cart').doc(id)
+          .update({
+            data: {
+              // 表示指示数据库将字段自增 1
+              num: _.inc(1)
+              //自减1为：_.inc(-1)
+            },
+            success: function (res) {
+              console.log('购物车数量+1')
+            },
+            fail:function(res){
+              console.log('购物车数量+1失败')
+            }
+          })
+        } 
+        //否则插入记录到购物车表:通过查询snack_id零食的信息res合并数量插入购物车记录
+        else {
+          db.collection('snacks').where({
+            _id: snack_id
+          }).get().then(res=>{ 
+            db.collection('cart').add({
+              data:{
+                snack_id: res.data[0]._id,
+                name: res.data[0].name,
+                price: res.data[0].price,
+                fileID: res.data[0].fileID,
+                num:1
+              }
+            }).then(res => {
+              console.log('成功加入购物车',res);
+            }).catch(err => {
+              console.log(err);
+            })
+          })
+        }
+      },
+      fail(err){
+        console.log(err);
+      }
+      })
+  },
+  
+//查询购物车  
+  selectCart:function(e){
+    wx.cloud.callFunction({
+      name: 'login',
+      success: res => {
+        db.collection('cart').where({
+          _openid: res.result.openid       
+        }).get()
+          .then(res => console.log(res))
+          .catch(err => console.error(err))
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
+    //------查询购物车:需要云函数
+    /*var $ = db.command.aggregate
+    db.collection('cart').aggregate()
+      .lookup({
+        from: "snacks",
+        localField: "snack_id",
+        foreignField: "_id",
+        as: "snackList"
+      })
+      .replaceRoot({
+        newRoot: $.mergeObjects([$.arrayElemAt(['$snackList', 0]), '$$ROOT'])
+      })
+      .project({
+        snackList: 0
+      })
+      .end()
+      .then(res => console.log(res))
+      .catch(err => console.error(err))*/
+  },
+
+  //清空购物车记录--云函数
+  removeAllCart: function (e) {
+    const _ = db.command
+    wx.cloud.callFunction({
+      name: 'cartDelete',
+      success: res => {
+        console.log('清空购物车',res)
+      },
+      fail: err => {
+        console.error('[云函数] [cartDelete] 调用失败', err)
       }
     })
   },
-
+  //删除一条购物车记录
+  removeCart: function (e) {
+    const _ = db.command
+    var _id = e.currentTarget.dataset.index     //前端的data-index
+    db.collection('cart')
+      .doc(_id)
+      .remove()
+    .then(res=>console.log('删除一条购物车记录',res))
+    .catch(err => console.error(err))
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  var _this=this
+  this.allType()
   },
 
   /**
